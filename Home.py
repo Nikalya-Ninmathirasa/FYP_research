6078aca146ca76360033bbde7319bf318f22cc0e
Nikalya_1398 <81620144+Nikalya-Ninmathirasa@users.noreply.github.com>
2023-07-02 23:35:13 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ from llama_index.query_engine import RetrieverQueryEngine
 
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-st.markdown('<p style="font-size:100px; color:black; text-align:center;">Chatbot for helping Hotel marketers for promoting their businesses in Galle District</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size:30px; color:black; text-align:center;">Chatbot for helping Hotel marketers for promoting their businesses in Galle District</p>', unsafe_allow_html=True)
 
 # Initialize pytrends
 pytrends = TrendReq(hl='en-US', tz=360)
@@ -139,6 +139,9 @@ ask = tab4.button("submit question")
 
 if ask:
     response = st.session_state.query_engine.query(ques)
-    st.write(response.response) 
+    if response is None:
+        st.write("Please provide more details about the question.")
+    else:
+        st.write(response.response)
 
 
