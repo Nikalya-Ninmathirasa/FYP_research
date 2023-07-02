2914479ed7e8f840df5850820d064cb62e37cc38
Nikalya_1398 <81620144+Nikalya-Ninmathirasa@users.noreply.github.com>
2023-07-03 00:20:42 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,7 +31,7 @@ st.markdown(
 )
 
 # Add the image with center alignment
-st.markdown('<div class="center"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNmbbuVqoA2mtNcl9VEKVXXoS4ZxXuI4y4CQ&usqp=CAU" alt="Image" width="200"></div>', unsafe_allow_html=True)
+st.markdown('<div class="center"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNmbbuVqoA2mtNcl9VEKVXXoS4ZxXuI4y4CQ&usqp=CAU" alt="Image" width="150"></div>', unsafe_allow_html=True)
 
 
 st.markdown('<p style="font-size:30px; color:black; text-align:center;">Chatbot for helping Hotel marketers for promoting their businesses in Galle District</p>', unsafe_allow_html=True)
