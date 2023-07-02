ec449810c1ddfac5504a64b562261d75995a9185
Nikalya_1398 <81620144+Nikalya-Ninmathirasa@users.noreply.github.com>
2023-07-03 00:15:04 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,9 +18,7 @@ from llama_index.query_engine import RetrieverQueryEngine
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 #image
-width = original_width * 0.5
-height = original_height * 0.5
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNmbbuVqoA2mtNcl9VEKVXXoS4ZxXuI4y4CQ&usqp=CAU", width=width, height=height)
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNmbbuVqoA2mtNcl9VEKVXXoS4ZxXuI4y4CQ&usqp=CAU", width=50)
 
 # Initialize pytrends
 
